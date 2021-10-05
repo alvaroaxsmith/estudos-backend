85d12f90e624b71aa353d0f29f47ecfeb48b63fd
alvaroun <alvaromachadoferreira@hotmail.com>
2021-10-05 20:16:54 -0300

    import const StatusCodes

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response, Router  } from "express";
+import { StatusCodes } from 'http-status-codes';
 
 // get /users
 // get /users/:uuid
@@ -10,12 +11,12 @@ const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) =>{
     const users = [{ userName: 'Alvaro' }];
-    res.status(200).send(users);
+    res.status(StatusCodes.OK).send(users);
 });
 
 usersRoute.get('/users/:uuid', (req: Request, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
-    res.status(200).send({ uuid });
+    res.status(StatusCodes.OK).send({ uuid });
 });
 
 export default usersRoute;
