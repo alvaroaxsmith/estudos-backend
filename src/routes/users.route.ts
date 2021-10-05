64d428ad1274b96b30fbb710c509d4269b181e9d
alvaroun <alvaromachadoferreira@hotmail.com>
2021-10-05 20:57:41 -0300

    Criado Delete

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -14,7 +14,7 @@ usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) =>{
     res.status(StatusCodes.OK).send(users);
 });
 
-usersRoute.get('/users/:uuid', (req: Request, res: Response, next: NextFunction) => {
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
     res.status(StatusCodes.OK).send({ uuid });
 });
@@ -24,6 +24,18 @@ usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
 
     console.log(req.body);
     res.status(StatusCodes.CREATED).send(newUser);
+});
+
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+
+    modifiedUser.uuid = uuid;
+    res.status(StatusCodes.OK).send(modifiedUser);
+});
+
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    res.sendStatus(StatusCodes.OK);
 });
 
 export default usersRoute;
