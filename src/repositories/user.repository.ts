75c97fe65e38d8211cf760df066d9e4fdbe15959
alvaroun <alvaromachadoferreira@hotmail.com>
2021-10-06 15:26:17 -0300

    created pool connections

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,10 +1,20 @@
-import User from "../models/user.model";
+import db from '../db';
+import User from '../models/user.model';
+
+
 
 class UserRepository {
 
 
-    findAllUsers(): User[] {
-        return [];
+    async findAllUsers(): Promise<User[]> {
+        const query = `
+            SELECT uuid, username
+            FROM application_user
+        `;
+
+        const result = await db.query<User>(query);
+        const rows = result.rows;
+        return rows || [];
     }
 }
 
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response, Router  } from "express";
 import { StatusCodes } from 'http-status-codes';
+import userRepository from "../repositories/user.repository";
 
 // get /users
 // get /users/:uuid
@@ -9,8 +10,8 @@ import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router();
 
-usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) =>{
-    const users = [{ userName: 'Alvaro' }];
+usersRoute.get('/users', async (req: Request, res: Response, next: NextFunction) =>{
+    const users = await userRepository.findAllUsers();
     res.status(StatusCodes.OK).send(users);
 });
 
